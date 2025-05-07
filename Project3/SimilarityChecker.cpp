9c945fe36e102e234a572711cca0e8ee5b937821
smax <dltmdals826@gmail.com>
2025-05-07 10:43:57 +0900

    refactoring

--- Project3/SimilarityChecker.cpp
+++ Project3/SimilarityChecker.cpp
@@ -3,28 +3,31 @@ using std::string;
 
 class SimChecker {
 private:
-	const int Perfect_Score_Legnth = 60;
+	const int MAX_SCORE_LENGTH = 60;
+	const int ZERO_SCORE = 0;
 public:
-	int lengthCheck(string str1, string str2) {
+	int lengthCheck(string& str1, string& str2) {
 		int length1 = str1.length();
 		int length2 = str2.length();
 
-		if (isMaxScore(length1, length2))
-			return Perfect_Score_Legnth;
+		if (isMaxScore(length1, length2)) return MAX_SCORE_LENGTH;
 
 		if (length2 > length1) {
-			int tmp;
-			tmp = length2;
+			auto tmp = length2;
 			length2 = length1;
 			length1 = tmp;
 		}
 
 		double gap = length1 - length2;
-		if (gap >= length2 * 2) return 0;
-				
-		double partialScore = (1 - (gap / length2)) * 60;
-		return partialScore;
+		if (isZeroScore(gap, length2)) return ZERO_SCORE;
+
+		return (1 - (gap / length2)) * MAX_SCORE_LENGTH;
 	}
+
+	bool isZeroScore(double gap, int length2) {
+		return gap >= length2 * 2;
+	}
+
 	bool isMaxScore(int length1, int length2) {
 		return length1 == length2;
 	}
